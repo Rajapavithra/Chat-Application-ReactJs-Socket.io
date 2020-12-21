e9c39df1f4d0a0580f0507be1255c8069a77acad
Rajapavithra <37566655+Rajapavithra@users.noreply.github.com>
2020-12-22 00:24:43 +0530

    order details uploaded

---
+++ client/src/components/OrderDetails/OrderDetails.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ProductCard from '../ProductCard/ProductCard';
+import './OrderDetails.css';
+
+const OrderDetails = (props)=>{
+    let {productDetails} = props;
+    return (
+        productDetails.map((product,key) =>{
+            let {
+                productName,
+                seller,
+                price,
+                color,
+                type,
+                DeliveryDate,
+                DeliveryCharge,
+                productImage
+            } = product;
+            return(
+                <ProductCard 
+                    key={`product_${key}`}
+                    avatarName={productImage}
+                    productName={productName}
+                    seller={seller}
+                    price={price}
+                    color={color}
+                    type={type}
+                    DeliveryDate={DeliveryDate}
+                    DeliveryFees={DeliveryCharge}
+                />
+            )
+        })
+    )
+}
+
+export default OrderDetails;
